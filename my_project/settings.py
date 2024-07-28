ebf9a4cf17e4a5f9211026bda70718ebd48e46d6
Oluwole Akani <oakani@gmail.com>
2024-07-28 09:26:33 +0000

    debug set to true

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hqkg-9%#k=eq-3wv(^4-!p1j(u6u&urqhurm6(iw1=_p32zait'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-oakani1-djangoproject-gds9n9yzyz5.ws.codeinstitute-ide.net','.herokuapp.com']
 
